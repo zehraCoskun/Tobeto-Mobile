c89de116ede8441fb4d99b24995fc46ab94dc1e6
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2023-12-14 19:35:51 +0300

    home drawer added

--- tobeto_mobil/lib/screens/home_screen.dart
+++ tobeto_mobil/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:tobeto_mobil/consts/constants.dart';
+import 'package:tobeto_mobil/widgets/home_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,83 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  UserAccountsDrawerHeader(
-                    accountName: const Text(
-                      'Zehra Coşkun',
-                      style: TextStyle(color: Colors.black87),
-                    ),
-                    accountEmail: const Text(
-                      'zehra@example.com',
-                      style: TextStyle(color: Colors.black87),
-                    ),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.background,
-                      child: Icon(
-                        Icons.person_2_outlined,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.onSecondary,
-                    ),
-                    onDetailsPressed: () {
-                      Navigator.of(context).pushNamed("/profile");
-                    },
-                  ),
-                  ListTile(
-                    title: const Text('🏠 Anasayfa'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    title: const Text('🖊️ Değerlendirmeler'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    title: const Text('😊 Profilim'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    title: const Text('📜 Katalog'),
-                    onTap: () {},
-                  ),
-                  ListTile(
-                    title: const Text('🗓️ Takvim'),
-                    onTap: () {},
-                  ),
-                  const Divider(),
-                  ListTile(
-                    title: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                      Image.asset(
-                        logoT,
-                        height: 22,
-                      ),
-                      const Text("  Tobeto")
-                    ]),
-                    onTap: () {},
-                  ),
-                  //ToggleButtons(children: [Icon(Icons.light_mode), Icon(Icons.dark_mode)], isSelected: [true, false]),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: padding16, vertical: padding32),
-                child: Image.asset(
-                  logo,
-                  height: padding32,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+      drawer: HomeDrawer(),
       appBar: AppBar(),
       body: Text("gfjhgkl"),
     );
