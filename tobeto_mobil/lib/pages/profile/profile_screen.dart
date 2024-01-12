1510dcf68424a13b4e16ed5d63bd28ece382fade
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-12 16:20:09 +0300

    profile updated

--- tobeto_mobil/lib/pages/profile/profile_screen.dart
+++ tobeto_mobil/lib/pages/profile/profile_screen.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tobeto_mobil/core/screens/global_scaffold.dart';
-import 'package:tobeto_mobil/pages/profile/certificates_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/competences_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/educations_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/experiences_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/languages_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/personal_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/settings_screen_widget.dart';
-import 'package:tobeto_mobil/pages/profile/social_media__screen_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -22,7 +14,7 @@ class _DenemeSayfasiState extends State<ProfileScreen> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 8, vsync: this);
+    _tabController = TabController(length: 1, vsync: this);
   }
 
   @override
@@ -44,13 +36,6 @@ class _DenemeSayfasiState extends State<ProfileScreen> with SingleTickerProvider
           isScrollable: true,
           tabs: const [
             Tab(text: 'Kişisel Bilgilerim'),
-            Tab(text: 'Deneyimlerim'),
-            Tab(text: 'Eğitim Hayatım'),
-            Tab(text: 'Yetkinliklerim'),
-            Tab(text: 'Sertifikalarım'),
-            Tab(text: 'Medya Hesaplarım'),
-            Tab(text: 'Yabancı Dillerim'),
-            Tab(text: 'Ayarlar')
           ],
         ),
       ),
@@ -71,16 +56,7 @@ class HomeTabbarView extends StatelessWidget {
   Widget build(BuildContext context) {
     return TabBarView(
       controller: _tabController,
-      children: const [
-        PersonalWidget(),
-        ExperiencesWidget(),
-        EducationsWidget(),
-        CompetencesWidget(),
-        CertificatesWidget(),
-        SocialMediaWidget(),
-        LanguagesWidget(),
-        SettingWidget(),
-      ],
+      children: const [Text("Profile")],
     );
   }
 }
