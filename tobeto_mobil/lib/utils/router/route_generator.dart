fa399cfca903ed575b9640dc50731847f78aa73a
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-12 16:22:36 +0300

    profile updated

--- tobeto_mobil/lib/utils/router/route_generator.dart
+++ tobeto_mobil/lib/utils/router/route_generator.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tobeto_mobil/pages/catalog/catalog_page.dart';
 import 'package:tobeto_mobil/pages/login/login_page.dart';
 import 'package:tobeto_mobil/pages/home/home_page.dart';
-import 'package:tobeto_mobil/pages/profile/profile_screen.dart';
 import 'package:tobeto_mobil/utils/router/route_transition.dart';
 
 class RouteGenerator {
@@ -15,7 +14,7 @@ class RouteGenerator {
       case "/home":
         return defaultRouteTransition(const HomePage());
       case "/profile":
-        return defaultRouteTransition(const ProfileScreen());
+      //return defaultRouteTransition(const ProfileScreen());
       case "/catalog":
         return defaultRouteTransition(const CatalogPage());
       default:
