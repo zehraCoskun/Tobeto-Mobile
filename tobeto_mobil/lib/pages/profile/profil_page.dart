f97e813dffb29f601693a98c84645978b93c5b51
Cem Güven <cemguven4108@gmail.com>
2024-01-15 17:40:58 +0300

    deleted the comment line

--- tobeto_mobil/lib/pages/profile/profil_page.dart
+++ tobeto_mobil/lib/pages/profile/profil_page.dart
@@ -63,7 +63,6 @@ class ProfilPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(16),
                   ),
                   child: const ContributionsGrid(),
-                  //asdasdasdads
                 )
               ],
             ),
