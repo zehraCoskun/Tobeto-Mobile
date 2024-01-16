2014b4c075503b68e663f57dc3f338a14c878af8
Zehra Coşkun <110024096+zehraCoskun@users.noreply.github.com>
2024-01-16 18:45:57 +0300

    wave animation created but not run

---
+++ tobeto_mobil/lib/pages/profile/profile_edit_page/animated_wave_curves.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:tobeto_mobil/pages/profile/profile_picture.dart';
+
+class MyAnimatedWaveCurves extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAnimatedWavesCurves();
+  }
+}
+
+class _MyAnimatedWavesCurves extends State<MyAnimatedWaveCurves> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: Duration(seconds: 4), vsync: this);
+    _controller.repeat();
+    animation = Tween<double>(begin: -600, end: 0).animate(_controller);
+    animation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.bottomCenter,
+        child: Stack(children: [
+          Positioned(
+            bottom: 0,
+            right: animation.value,
+            child: ClipPath(
+              clipper: MyWaveClipper(),
+              child: Opacity(
+                opacity: 0.5,
+                child: Container(
+                  color: Colors.deepPurple,
+                  width: 900,
+                  height: 200,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: animation.value,
+            child: ClipPath(
+              clipper: MyWaveClipper(),
+              child: Opacity(
+                opacity: 0.5,
+                child: Container(
+                  color: Colors.redAccent,
+                  width: 900,
+                  height: 200,
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class MyWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, 40.0);
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 40.0);
+
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        path.quadraticBezierTo(
+            size.width - (size.width / 16) - (i * size.width / 8), 0.0, size.width - ((i + 1) * size.width / 8), size.height - 160);
+      } else {
+        path.quadraticBezierTo(
+            size.width - (size.width / 16) - (i * size.width / 8), size.height - 120, size.width - ((i + 1) * size.width / 8), size.height - 160);
+      }
+    }
+
+    path.lineTo(0.0, 40.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+
+
+
+
+
+
+
+
--- tobeto_mobil/lib/pages/profile/profile_picture.dart
+++ tobeto_mobil/lib/pages/profile/profile_picture.dart
@@ -1,24 +1,33 @@
 //profil resmi
 import 'package:flutter/material.dart';
 import 'package:tobeto_mobil/constants/image_text.dart';
+import 'package:tobeto_mobil/pages/profile/profile_edit_page/animated_wave_curves.dart';
 
-class ProfilePicture extends StatelessWidget {
+class ProfilePicture extends StatefulWidget {
   const ProfilePicture({
     super.key,
   });
 
   @override
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
+
+class _ProfilePictureState extends State<ProfilePicture> {
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 32.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(60),
-        child: Image.asset(
-          ders1,
-          height: 120,
-          width: 120,
+      child: Stack(children: [
+        MyAnimatedWaveCurves(),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(60),
+          child: Image.asset(
+            ders1,
+            height: 120,
+            width: 120,
+          ),
         ),
-      ),
+      ]),
     );
   }
 }
