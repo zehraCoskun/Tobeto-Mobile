2b19fa6c34956533ffaa56788a23131669ce1c5a
Cem Güven <cemguven4108@gmail.com>
2024-02-16 14:12:55 +0300

    bloc builder added to home page

--- tobeto_mobil/lib/pages/home/home_page.dart
+++ tobeto_mobil/lib/pages/home/home_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tobeto_mobil/api/bloc/auth_bloc/auth_bloc.dart';
+import 'package:tobeto_mobil/api/bloc/auth_bloc/auth_state.dart';
+import 'package:tobeto_mobil/api/bloc/user_bloc/user_bloc.dart';
+import 'package:tobeto_mobil/api/bloc/user_bloc/user_event.dart';
+import 'package:tobeto_mobil/api/bloc/user_bloc/user_state.dart';
 import 'package:tobeto_mobil/core/screens/global_scaffold.dart';
+import 'package:tobeto_mobil/core/widgets/loading_state_widget.dart';
 import 'package:tobeto_mobil/core/widgets/secondary_background.dart';
 import 'package:tobeto_mobil/pages/home/home_body.dart';
 import 'package:tobeto_mobil/pages/home/home_header.dart';
@@ -11,7 +18,8 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
@@ -28,8 +36,29 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
         tabController: _tabController,
       ),
       body: SecondaryBackgroundWidget(
-        child: HomeBody(
-          controller: _tabController,
+        child: BlocConsumer<UserBloc, UserState>(
+          listener: (context, state) {
+            if (state is UserStateLoading) {
+              LoadingStateWidget.instance().show(
+                context: context,
+                text: "Loading...",
+              );
+            } else {
+              LoadingStateWidget.instance().hide();
+            }
+          },
+          builder: (context, state) {
+            if (state is UserStateInitial || state is UserStateUpdated) {
+              final state = context.read<AuthBloc>().state as AuthStateLoggedIn;
+              context.read<UserBloc>().add(UserEventFetch(id: state.user.uid));
+            }
+            if (state is UserStateFetched) {
+              return HomeBody(
+                controller: _tabController,
+              );
+            }
+            return Container();
+          },
         ),
       ),
     );
